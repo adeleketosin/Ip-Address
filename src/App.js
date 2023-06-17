b79be76490f9bab318e58064f43520186bf025b7
Oluwatosin Rhoda Adeleke <Adeleke.O@bullseye.local>
2023-06-17 16:52:43 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import { useState } from 'react';
-// import Header from './Header'
 import icon from './images/icon-arrow.svg'
 import './App.css';
 
 function App() {
-  const [key, setKey] = useState("")
-  const goat = (e) => {
-    e.preventdefault()
-    setKey(e.target.value)
+  const [level, setLevel] = useState("")
+  const [ip, setIp] = useState("")
+  const [location, setLocation] = useState('')
+  const [timeZone, setTimeZone] = useState('')
+  const [piAddress, setpiAddress] = useState('')
+
+  const list = (e) => {
+    // e.preventdefault()
+    setLevel(e.target.value)
+    console.log(e.target.value)
   }
-  // const ipAddress () =>{
-  //   fetch(`https://geo.ipify.org/api/v2/country?apiKey=${key}&ipAddress=8.8.8`)
-  // }
+  async function ipAddress(){
+    const data = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_3jdxe64utbNIhR5iYxrbNmwChSIop&ipAddress=${level}`)
+    const result = await data.json()
+    console.log(result)
+    setIp(result.isp)
+    setLocation(result.location.region)
+    setTimeZone(result.location.timezone)
+    setpiAddress(result.ip)
+
+
+  }
   return (
     <div className="App">
       <div className="header">
-        {/* https://geo.ipify.org/api/v2/country?apiKey=at_3jdxe64utbNIhR5iYxrbNmwChSIop&ipAddress=8.8.8.8 */}
-        <div>
-          <input type='search' className='search' onChange={goat}></input>
-          <img src={icon} className='icon' />
+       
+        <div className='iconn'>
+          <input type='search' className='search' onChange={list}></input>
+          <div className='ic'>
+            <img src={icon} className='icon' onClick={ipAddress}/>
+          </div>
         </div>
-        {/* <Header /> */}
       </div>
+      <h3>
+        {ip}
+      </h3>
+      <h3>{location}</h3>
+      <h3>{timeZone}</h3>
+      <h3>{piAddress}</h3>
     </div>
   );
 }
